c8a6f614053f88b9d9984e152ff86c3c0dc06be8
Homura-san <erickbispo@ufba.br>
2026-01-27 22:55:13 -0300

    Construindo os componentes

--- src/br/com/dio/UIMain.java
+++ src/br/com/dio/UIMain.java
@@ -1,17 +1,19 @@
 package br.com.dio;
 
-import br.com.dio.ui.custom.frame.MainFrame;
-import br.com.dio.ui.custom.panel.MainPanel;
+import br.com.dio.ui.custom.screen.MainScreen;
+
+import java.util.stream.Collectors;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.stream.Stream;
 
 public class UIMain {
     public static void main(String[] args) {
-        var dimension = new Dimension(600,600);
-        JPanel mainPanel = new MainPanel(dimension);
-        JFrame mainFrame = new MainFrame(dimension, mainPanel);
-        mainFrame.revalidate();
-        mainFrame.repaint();
+        var gameConfig = Stream.of(args)
+                .collect(Collectors.toMap(k -> k.split(";")[0], v -> v.split(";")[1]));
+
+        var mainScreen = new MainScreen(gameConfig);
+        mainScreen.buildMainScreen();
     }
 }
