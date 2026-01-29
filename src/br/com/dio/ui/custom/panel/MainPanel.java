d42404dc918ec2732f1b113848102ebc3e84b3b7
Homura-san <erickbispo@ufba.br>
2026-01-28 21:51:38 -0300

    Corrigindo e personalizando a tela do sudoku

--- src/br/com/dio/ui/custom/panel/MainPanel.java
+++ src/br/com/dio/ui/custom/panel/MainPanel.java
@@ -8,5 +8,6 @@ public class MainPanel extends JPanel {
     public MainPanel(final Dimension dimension){
         this.setSize(dimension);
         this.setPreferredSize(dimension);
+        this.setBackground(Color.GRAY);
     }
 }
--- src/br/com/dio/ui/custom/panel/SudokuSector.java
+++ src/br/com/dio/ui/custom/panel/SudokuSector.java
@@ -8,11 +8,12 @@ import java.awt.*;
 import java.util.List;
 
 import static java.awt.Color.black;
+import static java.awt.Color.yellow;
 
 public class SudokuSector extends JPanel {
 
     public SudokuSector(final List<NumberText> textFields){
-        var dimension = new Dimension(170,150);
+        var dimension = new Dimension(170,170);
         this.setSize(dimension);
         this.setPreferredSize(dimension);
         this.setBorder(new LineBorder(black, 2, true));
